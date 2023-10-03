3df1bba26994c93ee837c30f2026063d7bf45f50
Oguzhan <oguzhanzny@gmail.com>
2023-10-03 15:34:03 +0300

    add auth

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">H1-Bold</h1>
-      <h2 className="h2-bold">H2-Bold</h2>
-      <h3 className="h3-bold">H3-Bold</h3>
-    </div>
-  );
-}

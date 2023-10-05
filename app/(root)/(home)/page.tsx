53a8e046be3b857e79788eabc2c8cda76dbb26b9
Oguzhan <oguzhanzny@gmail.com>
2023-10-05 20:48:58 +0300

    NavBar complete, shadcn implement,  Theme complete, types and constants defined

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,10 +1,8 @@
-import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
       <h1>Home Page</h1>
-      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
